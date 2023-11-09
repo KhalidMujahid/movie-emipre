7276dd69d3a40d6104d08e05811ce8a862359d62
KhalidMujahid <binkhalid267@gmail.com>
2023-11-09 17:19:23 +0100

    update cloudinary code

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,15 @@
 import app from "./src/app";
 import dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
 import connectDB from "./src/configs/db";
 
 dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SEC,
+});
 
 const PORT: string | number = process.env.PORT || 3000;
 
