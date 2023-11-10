226f39164a003b507ef3643e31cd639707959cd5
Abed chinedu <chineduobaah@gmail.com>
2023-11-11 00:27:53 +0100

    ui updated

--- src/components/FileDetails.jsx
+++ src/components/FileDetails.jsx
@@ -1,10 +1,15 @@
 import React,{ useState,useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { XMarkIcon } from '@heroicons/react/20/solid'
+import fileSize from '../assets/file-size.png'
+
 
 function FileDetails() {
   const { id } = useParams();
   const [movie,setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading1, setIsLoading1] = useState(false);
 
   async function fetchMovieDetail(){
     const response = await axios.get(`https://movies-api-a6cx.onrender.com/api/movies/${id}`);
@@ -17,11 +22,178 @@ function FileDetails() {
     fetchMovieDetail();
   },[]);
 
+  const handleDownload = async () => {
+    try {
+      setIsLoading1(true);
+      const response = await fetch(movie.movie_file);
+      const blob = await response.blob();
+
+      // Create a link and trigger a download
+      const downloadLink = document.createElement('a');
+      downloadLink.href = window.URL.createObjectURL(blob);
+      downloadLink.download = `${movie.movie_title}(cinemax.com).mp4`;
+
+      // Append the link to the body
+      document.body.appendChild(downloadLink);
+
+      // Trigger a click on the link
+      downloadLink.click();
+
+      setIsLoading1(false);
+      // Remove the link from the body
+      document.body.removeChild(downloadLink);
+    } catch (error) {
+      console.error('Error during download:', error);
+    }
+  };
+
+
 
   return (
     <div>
-      <h2>File Details</h2>
-      <p>File ID: {id}</p>
+      <div className="flex items-center gap-x-6 bg-gray-900 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+          <p className="text-sm leading-6 text-white">
+            <a href="#">
+              <strong className="font-semibold">Cinemax.com</strong>
+              <svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
+                <circle cx={1} cy={1} r={1} />
+              </svg>
+              if your Download does not start automatically please click here&nbsp;&rarr;&nbsp;<a href={movie.movie_file} className=" text-cyan-600" aria-hidden="true">download</a>
+            </a>
+          </p>
+          <div className="flex flex-1 justify-end">
+            <button type="button" className="-m-3 p-3 focus-visible:outline-offset-[-4px]">
+              <span className="sr-only">Dismiss</span>
+              <XMarkIcon className="h-5 w-5 text-white" aria-hidden="true" />
+            </button>
+          </div>
+    </div>
+
+
+      {/* <h1>File Details</h1> */}
+      <div className="bg-white">
+      <div className="mx-auto px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        {/* Product */}
+        <div className="lg:grid lg:grid-cols-7 lg:grid-rows-1 lg:gap-x-8 lg:gap-y-10 xl:gap-x-16">
+          {/* Product image */}
+          <div className="lg:col-span-4 lg:row-end-1">
+            <div className="aspect-h-4 aspect-w-4 overflow-hidden rounded-lg bg-gray-100">
+              <img src="https://tailwindui.com/img/ecommerce-images/product-page-05-product-01.jpg" alt={movie.movie_title} className="object-cover object-center" />
+            </div>
+          </div>
+
+          {/* Product details */}
+          <div className="mx-auto mt-14 max-w-2xl sm:mt-16 lg:col-span-3 lg:row-span-2 lg:row-end-2 lg:mt-0 lg:max-w-none">
+            <div className="flex flex-col-reverse">
+              <div className="mt-4">
+                <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{movie.movie_title}_Cinemax.com.mp4</h1>
+
+                <h2 id="information-heading" className="sr-only">
+                  Product information
+                </h2>
+                <p className="mt-2 text-md text-gray-500">
+                  (Uploaded{' '}
+                                    <time dateTime={movie.updatedAt}>
+                                      {new Date(movie.updatedAt).toLocaleDateString(undefined, {
+                                        year: 'numeric',
+                                        month: 'short',
+                                        day: '2-digit',
+                                      })}
+                                    </time>
+                  )
+                </p>
+              </div>
+
+            </div>
+                <div className="flex items-center justify-between">
+                       <p className="mt-6 text-gray-500">{movie.movie_desc}</p>
+                       <div className="flex items-center">
+                            <img src={fileSize}className=" w-5 h-5" alt="file_size" />
+                            <h2 className="font-bold text-2xl">300mb</h2>
+                       </div>
+                      
+                </div>
+            
+
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-2">
+              <button
+                type="button"
+                className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+              >
+                Play
+              </button>
+                <button
+                    type="button"
+                    onClick={handleDownload}
+                    disabled={isLoading1}
+                    className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-50 px-8 py-3 text-base font-medium text-indigo-700 hover:bg-indigo-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+                  >
+                              {isLoading1 ? (
+                  <svg
+                    className="animate-spin -ml-1 mr-3 h-4 w-4 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 4.418 3.582 8 8 8v-4zm9.178-9.172a7.965 7.965 0 012.172 5.464h4c0-4.418-3.582-8-8-8v4zm-2 0a7.965 7.965 0 012.172-5.464V0c-4.418 0-8 3.582-8 8h4zm9.178 9.172a7.965 7.965 0 01-2.172 5.464V24c4.418 0 8-3.582 8-8h-4z"
+                    ></path>
+                  </svg>
+                ) : (
+                  'Download'
+                )}
+              </button>
+            </div>
+
+
+            <div className="mt-10 border-t border-gray-200 pt-10">
+              <div className="flex justify-evenly">
+              <h3 className="text-sm font-medium text-gray-900">Share</h3>
+              <h3 className="text-sm font-medium text-gray-900">Report</h3>
+              </div>
+              
+              <ul role="list" className="mt-4 flex items-center space-x-6">
+                <li>
+                  <a href="#" className="flex h-6 w-6 items-center justify-center text-gray-400 hover:text-gray-500">
+                    <span className="sr-only">Share on Facebook</span>
+                    <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                      <path
+                        fillRule="evenodd"
+                        d="M20 10c0-5.523-4.477-10-10-10S0 4.477 0 10c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V10h2.54V7.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V10h2.773l-.443 2.89h-2.33v6.988C16.343 19.128 20 14.991 20 10z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="flex h-6 w-6 items-center justify-center text-gray-400 hover:text-gray-500">
+                    <span className="sr-only">Share on Instagram</span>
+                    <svg className="h-6 w-6" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
+                      <path
+                        fillRule="evenodd"
+                        d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="flex h-6 w-6 items-center justify-center text-gray-400 hover:text-gray-500">
+                    <span className="sr-only">Share on Twitter</span>
+                    <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                      <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                    </svg>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
       {/* Display file details and download link */}
     </div>
   );
--- src/components/FileList.jsx
+++ src/components/FileList.jsx
@@ -8,6 +8,7 @@ function FileList() {
   async function fetchMovies(){
     const response = await axios.get("https://movies-api-a6cx.onrender.com/api/movies");
     setMovies(response.data);
+    console.log(response.data);
   }
 
   useEffect(() => {
@@ -34,11 +35,52 @@ function FileList() {
         </div>
       </div>
     </div>
-      {/* Display a list of uploaded files */}
-      {movies.map(movie => (
-        //movies card here
-        <div></div>
-      ))}
+        <div className="bg-white">
+      <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+        <div className="sm:flex sm:items-baseline sm:justify-between">
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">Movie Lists</h2>
+          <a href="#" className="hidden text-sm font-semibold text-indigo-600 hover:text-indigo-500 sm:block">
+            Browse Recent
+            <span aria-hidden="true"> &rarr;</span>
+          </a>
+        </div>
+
+        <div className="mt-6 grid grid-cols-1 gap-y-10 sm:grid-cols-3 sm:gap-x-6 sm:gap-y-0 lg:gap-x-8"
+        >
+          {movies.map((movie) => (
+            <div key={movie._id} className="group relative" 
+            >
+              <div className="h-96 w-full overflow-hidden rounded-lg sm:aspect-h-3 sm:aspect-w-2 group-hover:opacity-75 sm:h-auto">
+              <Link to={`/file/${movie._id}`} className="h-96 w-full overflow-hidden rounded-lg sm:aspect-h-3 sm:aspect-w-2 group-hover:opacity-75 sm:h-auto">
+                  <img
+                    // src={movie.movie_image_back}
+                    src={`https://ui-avatars.com/api/?name=${movie.movie_title}`}
+                    alt={movie.movie_title}
+                    className="h-full w-full object-cover object-center"
+                  />
+                </Link>
+              </div>
+              <h3 className="mt-4 text-base font-semibold text-gray-900">
+                  <Link to={`/file/${movie._id}`}>
+                    <span className="absolute inset-0" />
+                    {movie.movie_title}
+                  </Link>
+              </h3>
+              <p className="mt-1 text-sm text-gray-500">{movie.movie_desc}</p>
+              <p className="mt-1 text-sm text-gray-500">Uploaded at:{movie.updatedAt}</p>
+              <p className="mt-1 text-sm text-gray-500 hidden">Download Link:{movie.movie_file}</p>
+            </div>
+          ))}
+        </div>
+
+        <div className="mt-6 sm:hidden">
+          <a href="#" className="block text-sm font-semibold text-indigo-600 hover:text-indigo-500">
+            Browse all favorites
+            <span aria-hidden="true"> &rarr;</span>
+          </a>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
